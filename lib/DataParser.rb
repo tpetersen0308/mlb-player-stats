662713709b19b521d435b37d91fd3493b69f76ec
tpetersen0308 <travis.e.petersen@gmail.com>
2018-05-15 16:24:01 -0700

    add stats parsing methods to data parser

--- lib/DataParser.rb
+++ lib/DataParser.rb
@@ -16,4 +16,28 @@ class DataParser
 
     return players
   end
+
+  def calculate_k_pct(player)
+    return (((player['stats']['PitcherStrikeouts']['#text'].to_f)/(player['stats']['TotalBattersFaced']['#text'].to_f))*100).round(1).to_s
+  end
+
+  def calculate_bb_pct(player)
+    return (((player['stats']['PitcherWalks']['#text'].to_f)/(player['stats']['TotalBattersFaced']['#text'].to_f))*100).round(1).to_s
+  end
+
+  def calculate_gb_pct(player)
+    return (((player['stats']['PitcherGroundBalls']['#text'].to_f)/(player['stats']['PitchesThrown']['#text'].to_f))*100).round(1).to_s
+  end
+
+  def calculate_fb_pct(player)
+    return (((player['stats']['PitcherFlyBalls']['#text'].to_f)/(player['stats']['PitchesThrown']['#text'].to_f))*100).round(1).to_s
+  end
+
+  def get_era(player)
+    return player['stats']['EarnedRunAvg']['#text']
+  end
+
+  def calculate_swstr_pct(player)
+    return (((player['stats']['PitcherStrikesMiss']['#text'].to_f)/(player['stats']['PitchesThrown']['#text'].to_f))*100).round(1).to_s
+  end
 end
