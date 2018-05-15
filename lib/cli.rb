227d51d30bb24895d3ba093e26897c120005ab75
tpetersen0308 <travis.e.petersen@gmail.com>
2018-05-15 16:41:10 -0700

    add start method to control CLI flow

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,6 +16,13 @@ class CLI
     return players.join(",")
   end
 
-
+  def start
+    puts "Welcome to MLB Player Stats"
+    player_data = DataRetriever.new.get_player_data(get_players)
+    players_hash = DataParser.new.create_players_hash(player_data)
+    # call method to export data from players_hash to excel sheet
+    puts "Your player stats have been exported to <filename>."
+    puts "Have a nice day!"
+  end
 
 end
